1dbd07f5383eccdccfd0de092bf48ddf224774ae
XG <wangyifan@tbs-info.com>
2019-06-12 13:58:50 +0800

    [feat](All):[完善gradle配置，使应用模块test与框架模块framework可以连通]

---
+++ framework/src/main/java/com/xg/starter/MiniApplication.java
@@ -0,0 +1,14 @@
+package com.xg.starter;
+
+public class MiniApplication
+{
+    /**
+     * 框架入口类
+     * @param clazz 应用的入口类
+     * @param args 应用入口类的启动参数 数组
+     */
+    public static void run(Class<?> clazz, String[] args)
+    {
+        System.out.println("fuck mini");
+    }
+}
---
+++ test/src/main/java/com.xg/Application.java
@@ -0,0 +1,12 @@
+package com.xg;
+
+import com.xg.starter.MiniApplication;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("fuck huh");
+        MiniApplication.run(Application.class, args);
+    }
+}
